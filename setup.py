ba444f89fbd384c0eaed1670d2d39ef7d76e333a
Keagan McNew <kmcnew2@gmail.com>
2020-10-31 13:17:03 -0500

    initial commit

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
-setup(name='McUtilize',
+setup(
+name='McUtilize',
 version='0.1',
 description='General utilities for various tasks',
-url='#',
+url='https://github.com/KeaganM/McUtilize',
 author='Keagan McNew',
 author_email='kmcnew2@gmail.com',
 license='MIT',

91c241a2f0ccf35adbc19625446356148c7c8f1d
Keagan McNew <kmcnew2@gmail.com>
2020-11-21 18:40:21 -0600

    Add path_maps_handler
    - Add handler to accept a path mapping
    that only contains names and set it up to find
    files based on file name only.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 setup(
 name='McUtilize',
-version='0.1',
+version='0.2',
 description='General utilities for various tasks',
 url='https://github.com/KeaganM/McUtilize',
 author='Keagan McNew',
